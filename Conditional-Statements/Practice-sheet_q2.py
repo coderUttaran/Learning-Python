4daee2a872d09209fa63f3f8af0aadbad2315b5d
coderUttaran <dasuttaran97@gmail.com>
2024-12-26 01:39:44 -0800

    Modifify Practice sheet Q2

--- Conditional-Statements/Practice-sheet_q2.py
+++ Conditional-Statements/Practice-sheet_q2.py
@@ -1,14 +1,16 @@
 mark1 = int(input("Enter the marks of first subject: "))
 mark2 = int(input("Enter the marks of second subject: "))
 mark3 = int(input("Enter the marks of third subject: "))
-total_percentage = ((mark1 + mark2 + mark3) / 300)*100
 
-if(total_percentage >=40):
-    if(mark1 > 100 or mark2 > 100 or mark3 > 100):
-        print("Invalid marks")
-    elif(mark1 < 33 or mark2 < 33 or mark3 < 33):
-        print(f"You are failed because of scoring below 33 in the subjects with: {total_percentage}% ")
-    elif(mark1 >= 33 and mark2 >= 33 and mark3 >= 33):
-        print("You are passed with: ", total_percentage, "%")
+if mark1 > 100 or mark1 < 0 or mark2 > 100 or mark2 < 0 or mark3 > 100 or mark3 < 0:
+    print("Invalid marks entered, please enter marks between 0 and 100")
 else:
-    print("You are failed with: ", total_percentage, "%")
+    total_marks = mark1 + mark2 + mark3
+    total_percentage = (total_marks / 300) * 100
+
+    if(mark1 < 33 or mark2 < 33 or mark3 < 33):
+        print("You have failed in the exam because you have scored less than 33% in one of the subjects")
+    elif(total_percentage < 40):
+        print("You have failed in the exam because you have scored less than 40% in total")
+    else:
+        print(f"Congratulations! You have passed the exam with {total_percentage}%")
